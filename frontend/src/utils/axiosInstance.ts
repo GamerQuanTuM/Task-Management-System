3dd2a235b74e4229a4a68cd77cc819b4fdc6cd35
GamerQuanTuM <shuvamsantra10@gmail.com>
2025-01-25 13:29:20 +0530

    Changed axios

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// Create an Axios instance with a base URL
 export const axiosInstance = axios.create({
-  baseURL: "https://task-management-system-w0tv.onrender.com/tasks", // Replace with your base URL
+  baseURL: "https://task-management-system-w0tv.onrender.com/tasks",
 });
